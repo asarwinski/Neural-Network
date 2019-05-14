8a548568d3d1ce1801a5e53f712cc3fedb7c6f0a
asarwinski <47275169+asarwinski@users.noreply.github.com>
2019-05-14 09:32:15 +0200

    Adding many functions to neuron class

--- NeuralNetworkClassLib/Neuron.cs
+++ NeuralNetworkClassLib/Neuron.cs
@@ -8,5 +8,48 @@ namespace NeuralNetworkClassLib
 {
     class Neuron
     {
+        List<double> weights = new List<double>();
+        IActivationFunction activationFunction;
+        
+        static Random rnd = new Random();
+        
+        public double _value;
+        public double Value 
+        {      
+            get
+            {
+                return _value;   
+            }
+            private set
+            {
+                PreviousValue = _value;
+                _value = value;
+            }
+        }
+        public double PreviousValue { get; private set; }
+        
+        public Neuron(int numberOfInputs, IActivationFunciotn activationFunction)
+        {
+            InitalizeWeights(numberOfInputs);
+            this.activaionFunction = activationFunction;
+        }
+        
+        public void PushValues(List<double> inputValues)
+        {
+            double weightedSum;
+            inputValues.Insert(1,0);
+            weightedSum = inputValues.ForEach(value => value*weights[inputValues.IndexOf(value)]);
+            Value = activationFuntion(weightedSum);
+        }
+        
+        InitalizeWeights(int numberOfInputs)
+        {
+            //need one more weight for bias
+            for(int i = 0; i < numberOfInputs+1; i++)
+            {
+                double newWeight = rnd.NextDouble()*10-5;
+                weights.Add(newWeight); 
+            }
+        }
     }
 }
