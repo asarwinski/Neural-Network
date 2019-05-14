53fdfa5c6ce58e86c541a15c4e59bdc14a78f827
Adrian Sarwiński <sarwinski.adrian@gmail.com>
2019-05-14 19:35:15 +0200

    Added IActivationFunction and bunch of stuff

--- NeuralNetworkClassLib/Classes/Neuron.cs
+++ NeuralNetworkClassLib/Classes/Neuron.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NeuralNetworkClassLib.Interfaces;
 
 namespace NeuralNetworkClassLib
 {
@@ -28,23 +29,23 @@ namespace NeuralNetworkClassLib
         }
         public double PreviousValue { get; private set; }
         
-        public Neuron(int numberOfInputs, IActivationFunciotn activationFunction)
+        public Neuron(int numberOfInputs, IActivationFunction activationFunction)
         {
             InitalizeWeights(numberOfInputs);
-            this.activaionFunction = activationFunction;
+            this.activationFunction = activationFunction;
         }
         
         public void PushValues(List<double> inputValues)
         {
             double weightedSum;
+            //need one more weight for bias
             inputValues.Insert(0,1);
-            weightedSum = inputValues.ForEach(value => value*weights[inputValues.IndexOf(value)]);
-            Value = activationFuntion(weightedSum);
+            weightedSum = inputValues.Select(value => value * weights[inputValues.IndexOf(value)]).Sum();
+            Value = activationFunction.Calculate(weightedSum);
         }
         
-        InitalizeWeights(int numberOfInputs)
-        {
-            //need one more weight for bias
+        void InitalizeWeights(int numberOfInputs)
+        {    
             for(int i = 0; i < numberOfInputs+1; i++)
             {
                 double newWeight = rnd.NextDouble()*10-5;
---
+++ NeuralNetworkClassLib/Interfaces/IActivationFunction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetworkClassLib.Interfaces
+{
+    interface IActivationFunction
+    {
+        double Calculate(double input);
+        double GetDerivative(double input);
+    }
+}
